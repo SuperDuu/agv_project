c925e37a27f3f7079ee4475b2e60fdcd5395b973
SuperDuu <vuducdu2k5@gmail.com>
2026-07-09 09:29:57 +0700

    implement arm parser reset

--- arm/arm_firmware_right/Core/Src/main.c
+++ arm/arm_firmware_right/Core/Src/main.c
@@ -108,7 +108,7 @@ static void MX_TIM11_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
-//static void ARM_Proto_ResetParser(void);
+static void ARM_Proto_ResetParser(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -164,7 +164,7 @@ int main(void)
   Encoder_Init();
 
 //  JointControl_Init();
-//  ARM_Proto_ResetParser();
+  ARM_Proto_ResetParser();
   HAL_UART_Receive_IT(&huart1, &arm_rx_byte, 1);
   /* USER CODE END 2 */
 
@@ -1043,14 +1043,27 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
             break;
         }
 
-        HAL_UART_Receive_IT(&huart1, &arm_rx_byte, 1);
+        if (HAL_UART_Receive_IT(&huart1, &arm_rx_byte, 1) != HAL_OK) {
+            HAL_UART_AbortReceive(&huart1);
+            HAL_UART_Receive_IT(&huart1, &arm_rx_byte, 1);
+        }
     }
+}
+
+static void ARM_Proto_ResetParser(void) {
+    arm_frame_idx    = 0;
+    arm_expected_len = 0;
+    arm_parser_state = 0;
 }
 
 void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
     if (huart->Instance == USART1) {
         __HAL_UART_CLEAR_OREFLAG(huart);
-        HAL_UART_Receive_IT(huart, &arm_rx_byte, 1);
+        ARM_Proto_ResetParser();
+        if (HAL_UART_Receive_IT(huart, &arm_rx_byte, 1) != HAL_OK) {
+            HAL_UART_AbortReceive(huart);
+            HAL_UART_Receive_IT(huart, &arm_rx_byte, 1);
+        }
     }
 }
 /* USER CODE END 4 */
