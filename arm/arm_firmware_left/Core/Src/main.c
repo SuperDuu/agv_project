7a6c4abdea8c9860b0be65f4ddb8c009151a251a
SuperDuu <vuducdu2k5@gmail.com>
2026-07-10 15:38:25 +0700

    invert left servo directions

--- arm/arm_firmware_left/Core/Src/main.c
+++ arm/arm_firmware_left/Core/Src/main.c
@@ -965,8 +965,8 @@ static void ARM_Proto_ProcessFrame(const uint8_t *frame, uint16_t frame_len) {
 //        servo_deg[i] = ARM_Proto_X100ToDeg(q_new[i]);
 //      }
         servo_deg[1] = -ARM_Proto_X100ToDeg(q_new[1])+90;
-        servo_deg[2] = ARM_Proto_X100ToDeg(q_new[2])+35;
-        servo_deg[3] = 65-ARM_Proto_X100ToDeg(q_new[3]);
+        servo_deg[2] = -ARM_Proto_X100ToDeg(q_new[2])+35;
+        servo_deg[3] = 65+ARM_Proto_X100ToDeg(q_new[3]);
         servo_deg[4] = ARM_Proto_X100ToDeg(q_new[4])+90;
         servo_deg[5] = ARM_Proto_X100ToDeg(q_new[5])+96.43;
       /* Update last accepted position */
@@ -1040,8 +1040,8 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 //                    servo_deg[i] = (float)q[i] / 100.0f;
 //                  }
                   servo_deg[1] =-(float)q[1] / 100.0f+90;
-                  servo_deg[2] = (float)q[2] / 100.0f+35;
-                  servo_deg[3] = 65.0f-(float)q[3] / 100.0f;
+                  servo_deg[2] = -(float)q[2] / 100.0f+35;
+                  servo_deg[3] = 65.0f+(float)q[3] / 100.0f;
                   servo_deg[4] = (float)q[4] / 100.0f+90;
                   servo_deg[5] = (float)q[5] / 100.0f+96.43;
                 } else {
